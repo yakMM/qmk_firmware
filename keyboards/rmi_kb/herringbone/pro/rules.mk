152c7a62f81b5b103b2a6992f9378be4f902bc65
YakMM <yak.mayam@gmail.com>
2023-10-22 16:47:15 +0200

    Personnal herringbone keymap

--- keyboards/rmi_kb/herringbone/pro/rules.mk
+++ keyboards/rmi_kb/herringbone/pro/rules.mk
@@ -4,18 +4,18 @@ F_CPU = 16000000
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
-OLED_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+OLED_ENABLE = no
 WPM_ENABLE = yes
 LTO_ENABLE = yes
-
-SRC += pattern.c
+COMBO_ENABLE = yes
